c4c56092dcb0f402222cf9b06c9d5c952e408b75
vinayg2509 <vinayg2509@gmail.com>
2025-11-27 21:06:12 +0530

    [Vinay] Added header and footer without any validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/layouts/header/header.component";
+import { FooterComponent } from "./shared/layouts/footer/footer.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
