ab2e7181da9436bab02207f359271f5a1a56639c
vinayg2509 <vinayg2509@gmail.com>
2025-11-29 10:57:39 +0530

    [Vinay]Added service for user login and register

--- src/app/core/service/api.service.ts
+++ src/app/core/service/api.service.ts
@@ -1,9 +1,75 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  // Default HTTP headers used for all API requests
+  httpOption = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      // Note: This header does NOT fix CORS issues on client side.
+      // CORS must be enabled on server.
+      'Access-Control-Allow-Access': '*',
+    }),
+  };
 
-  constructor() { }
+  constructor(private httpMethod: HttpClient) {}
+
+  /**
+   * Handle API errors and return a readable error message.
+   * @param error - Error returned from backend
+   * @returns Observable that throws an error
+   */
+  private formatError(error: any) {
+    return throwError(error.error);
+  }
+
+  /**
+   * GET Request Wrapper
+   * @param path - API endpoint URL
+   * @param params - Optional query params
+   * @returns Observable with API response
+   */
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.httpMethod
+      .get(path, { params })
+      .pipe(catchError(this.formatError));
+  }
+  /**
+   * PUT Request Wrapper
+   * @param path - API endpoint URL
+   * @param body - Request payload (object that will be updated on server)
+   * @returns Observable with API response
+   */
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.httpMethod
+      .put(path, JSON.stringify(body), this.httpOption) // Send payload as JSON
+      .pipe(catchError(this.formatError)); // Handle API errors
+  }
+
+  /**
+   * POST Request Wrapper
+   * @param path - API endpoint URL
+   * @param body - Request payload that will be sent to server
+   * @returns Observable with API response
+   */
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.httpMethod
+      .post(path, JSON.stringify(body), this.httpOption) // Convert payload to JSON string
+      .pipe(catchError(this.formatError)); // Handle errors globally
+  }
+
+  /**
+   * DELETE Request Wrapper
+   * @param path - API URL of the item to delete
+   * @returns Observable with API response
+   */
+  delete(path: string): Observable<any> {
+    return this.httpMethod
+      .delete(path, this.httpOption) // Send delete request
+      .pipe(catchError(this.formatError)); // Error handling
+  }
 }
--- src/app/shared/services/login-signup.service.ts
+++ src/app/shared/services/login-signup.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginSignupService {
+  public login_url = 'http://localhost:3000';
+  public reg_url = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient, private apiService: ApiService) {}
+
+  authLogin(user_email: any, password: any): Observable<any> {
+    return this.apiService.get(
+      this.login_url + '/user?email=' + user_email + '&password=' + password
+    );
+  }
+  userRegister(user_details:any):Observable<any>{
+    return this.apiService.put(this.reg_url+'/user='+user_details)
+  }
+
+  adminLogin(user_email:any,password:any):Observable<any>{
+    return this.apiService.get(this.login_url+'/user?email='+user_email+'&password='+password+'&role=admin')
+  }
+
+
 }
