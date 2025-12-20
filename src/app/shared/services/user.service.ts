7cb6f5f481e0f4a3af0f0773d2eedc2c2ab461db
vinayg2509 <vinayg2509@gmail.com>
2025-12-20 10:47:22 +0530

    [Vinay] design profile update form using reactive forms

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,8 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { ApiService } from '../../core/service/api.service';
-import { User } from '../../core/Model/object-model';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,16 +8,16 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  public user_url="http://localhost:3000/user/"
-  constructor(private http:HttpClientModule,private apiService:ApiService) { }
+  public user_url ="http://localhost:3000/users/";
 
-  // get user
-  getUserData(user_id:any)
-  {
-    return this.apiService.get(this.user_url+user_id)
+  constructor(private http:HttpClient, private apiService:ApiService) { }
+
+  //get individual data
+  getUserData(user_id:number){
+    return this.apiService.get(this.user_url+user_id);
   }
-  updateUserData(user_id:any,user_details:User):Observable<any>
-  {
-    return this.apiService.put(this.user_url+user_id,user_details)
+  //update data by user_id
+  updateUserData(user_id:any, user_dto:any):Observable<any>{
+    return this.apiService.put(this.user_url+user_id, user_dto);
   }
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,130 +1,130 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UserService } from '../shared/services/user.service';
 import { User } from '../core/Model/object-model';
 import { Router } from '@angular/router';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss',
+  styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent implements OnInit{
+export class UserProfileComponent implements OnInit {
   userProfileForm!: FormGroup;
-  userProfile:boolean=false
-  user_data:any
-  user_update_data!:any
-  user_details!: User;
-  user_id!: any;
-  user_profile_pic!:any
-  user_languege:any
-  user_role:any
+  userProfile: boolean = false;
+  user_id!: number;
+  user_data: any;
+  user_update_data: any;
+  user_dto!: User;
+  user_profile_pic: any;
+  user_language: any;
+  user_role: any;
 
-  constructor(private userService: UserService,private router:Router,private formBuilder:FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private router: Router, private user_Service: UserService) { }
+
   ngOnInit(): void {
-    this.user_id=Number(sessionStorage.getItem('user_session_id'))
-    this.userProfileForm=this.formBuilder.group({
-        name: ['', Validators.required],
-            mobNumber: ['', Validators.required],
-            age: ['', Validators.required],
-            dob: ['', Validators.required],
-            email: ['', Validators.required],
-            password: ['', Validators.required],
-            addressLine1: ['', Validators.required],
-            addressLine2: ['', Validators.required],
-            city: ['', Validators.required],
-            state: ['', Validators.required],
-            zipCode: ['', Validators.required],
-            language: ['', Validators.required],
-            gender: ['', Validators.required],
-            aboutYou: ['', Validators.required],
-            uploadPhoto: ['', Validators.required],
-            role: ['', Validators.required],
-            agreetc: ['', Validators.required],
-    })
-    this.editUserData(this.user_id)
+    this.user_id = Number(sessionStorage.getItem('user_session_id'));
+    
+    this.userProfileForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      mobNumber: ['', Validators.required],
+      age: ['', Validators.required],
+      dob: ['', Validators.required],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      addressLine1: ['', Validators.required],
+      addressLine2: [],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      language: ['', Validators.required],
+      gender: ['', Validators.required],
+      aboutYou: ['', Validators.required],
+      uploadPhoto: ['', Validators.required],
+    });
+
+    this.editUserData(this.user_id);
   }
-  get rf()
-  {
+
+  get rf() {
     return this.userProfileForm.controls;
   }
+
   editUserData(user_id: any) {
-    this.userService.getUserData(this.user_id).subscribe(data=>{
-      this.user_data=data;
-      this.user_profile_pic=this.user_data.uploadPic
-      this.user_languege=this.user_data.language
-      this.user_role=this.user_data.role
-      this.userProfileForm.setValue({
-         name: this.user_data.name,
-      mobNumber: this.user_data.mobNumber,
-      age: this.user_data.age,
-      dob: this.user_data.dob,
-      email: this.user_data.email,
-      password: this.user_data.password,
-      address: {
-        addressLine1: this.user_data.address.addressLine1,
-        addressLine2: this.user_data.address.addressLine2,
-        city: this.user_data.address.city,
-        state: this.user_data.address.state, 
-        zipCode: this.user_data.address.zipCode,
-      },
-    
-      gender: this.user_data.gender,
-      aboutYou: this.user_data.aboutYou,
-   
-      agreetc: this.user_data.agreetc,
-      })
-    },error=>{
-      console.log("My Error",error);
-      
-    })
+    this.user_Service.getUserData(user_id).subscribe(data => {
+      this.user_data = data;
+      this.user_profile_pic = this.user_data.uploadPhoto;
+      this.user_language = this.user_data.language;
+      this.user_role = this.user_data.role;
+
+      // Using patchValue to fill the form
+      // The ?. (Optional Chaining) is critical to prevent "Not Found" errors if address is null
+      this.userProfileForm.patchValue({
+        name: this.user_data.name,
+        mobNumber: this.user_data.mobNumber,
+        age: this.user_data.age,
+        dob: this.user_data.dob,
+        email: this.user_data.email,
+        password: this.user_data.password,
+        language: this.user_data.language,
+        gender: this.user_data.gender,
+        addressLine1: this.user_data.address?.addressLine1,
+        addressLine2: this.user_data.address?.addressLine2,
+        city: this.user_data.address?.city,
+        state: this.user_data.address?.state,
+        zipCode: this.user_data.address?.zipCode,
+        aboutYou: this.user_data.aboutYou,
+        uploadPhoto: '' // Keep as empty string so the validator doesn't block update unless user picks new file
+      });
+    }, error => {
+      console.log("My error", error);
+    });
   }
 
+  updateProfile() {
+    this.userProfile = true;
+    if (this.userProfileForm.invalid) {
+      return;
+    }
 
-  updateUserData()
-  {
-    this.userProfile=true;
-    if(this.userProfileForm.invalid)return
-
-    this.user_update_data=this.userProfileForm.value
-    this.user_details={
-       name: this.user_update_data.name,
-      mobNumber: this.user_update_data.mobNumber,
+    this.user_update_data = this.userProfileForm.value;
+    this.user_dto = {
+      aboutYou: this.user_update_data.aboutYou,
       age: this.user_update_data.age,
+      agreetc: this.user_update_data.agreetc,
       dob: this.user_update_data.dob,
       email: this.user_update_data.email,
-      password: this.user_update_data.password,
+      gender: this.user_update_data.gender,
       address: {
-        id:0,
+        id: 0,
         addressLine1: this.user_update_data.addressLine1,
         addressLine2: this.user_update_data.addressLine2,
         city: this.user_update_data.city,
         state: this.user_update_data.state,
         zipCode: this.user_update_data.zipCode,
-        
       },
       language: this.user_update_data.language,
-      gender: this.user_update_data.gender,
-      aboutYou: this.user_update_data.aboutYou,
-      uploadPhoto: this.user_update_data.uploadPhoto ? this.user_profile_pic:this.user_update_data.uploadPhoto,
-      role: this.user_update_data.role,
-      agreetc: this.user_update_data.agreetc,
-    }
-    this.userService.updateUserData(this.user_id,this.user_details).subscribe(data=>{
-      alert("Profile Updated Succesfully")
-      if(this.user_role=='admin')
-        this.router.navigateByUrl('admin/dashboard')
-      else if (this.user_role=='buyer')
-        this.router.navigateByUrl('buyer/dashboard')
-      else if(this.user_role=='seller')
-        this.router.navigateByUrl('seller/dashboard')
-    },error=>{
-      console.log("My Error",error);
-      
-    })
+      mobNumber: this.user_update_data.mobNumber,
+      name: this.user_update_data.name,
+      password: this.user_update_data.password,
+      uploadPhoto: (this.user_update_data.uploadPhoto == "" ? this.user_profile_pic : this.user_update_data.uploadPhoto),
+      role: this.user_role // Using user_role from editUserData
+    };
 
+    this.user_Service.updateUserData(this.user_id, this.user_dto).subscribe(data => {
+      alert("Profile Update Successful !");
+      if (this.user_role == 'admin') {
+        this.router.navigateByUrl('/admin-dashboard');
+      } else if (this.user_role == 'seller') {
+        this.router.navigateByUrl('/seller-dashboard');
+      } else if (this.user_role == 'buyer') {
+        this.router.navigateByUrl('/buyer-dashboard');
+      }
+    }, error => {
+      console.log("My error", error);
+    });
   }
 }
